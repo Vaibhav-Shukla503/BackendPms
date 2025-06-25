ca9de97948de06f3add03b2f9ea6f81883f21bee
Vaibhav Shukla <vaibhavshuklanitdgp48@gmail.com>
2025-06-25 13:32:48 -0400

    added file

--- src/main/java/com/proj/proj/Repository/PropertyRepository.java
+++
@@ -1,8 +0,0 @@
-// src/main/java/com/example/propertymgmt/repository/PropertyRepository.java
-package com.proj.proj.Repository;
-
-import com.proj.proj.Model.Property;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PropertyRepository extends JpaRepository<Property, Long> {
-}
